4ce881c5d246acca6714324fefce84e87501f8c1
Steckij Nikita <stets119@gmail.com>
2023-10-28 22:51:23 +0300

    index.ts file added

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import Button from "./components/Button";
+import FacebookAuthButton from "./components/FacebookAuthButtton";
+import FileInput from "./components/FileInput";
+import GitHubAuthButton from "./components/GitHubAuthButton";
+import GoogleAuthButton from "./components/GoogleAuthButton";
+import HorizontalCarousel from "./components/HorizontalCarousel";
+import Icon from "./components/Icon";
+import Input from "./components/Input";
+import PasswordInput from "./components/PasswordInput";
+import RadioButton from "./components/RadioButton";
+import Review from "./components/Review";
+import Seat from "./components/Seat";
+import Session from "./components/Session";
+import VideoPlayer from "./components/VideoPlayer";
+
+export {
+  Button,
+  FacebookAuthButton,
+  FileInput,
+  GitHubAuthButton,
+  GoogleAuthButton,
+  HorizontalCarousel,
+  Icon,
+  Input,
+  PasswordInput,
+  RadioButton,
+  Review,
+  Seat,
+  Session,
+  VideoPlayer
+};
